8bd800c2f2436a863741f2b0dcbe1217dbc95bf0
Eemeli Hayrynen <emppuposti@outlook.com>
2023-02-11 01:22:39 +0200

    0.5 class works with lazycolumn, todo: edit and delete from class with use of timepicker ui

--- app/src/main/java/com/example/mobile/MainActivity3.kt
+++ app/src/main/java/com/example/mobile/MainActivity3.kt
@@ -6,18 +6,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.ui.unit.dp
+import com.example.mobile.repository.reminderRepository
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.ui.Modifier
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-
-
+import com.example.mobile.ui.theme.MobileTheme
 
 class MainActivity3 : ComponentActivity() {
 
@@ -25,91 +25,70 @@ class MainActivity3 : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Scaffold(
-                topBar = {
-                    TopAppBar(
+            val remRep = reminderRepository()
+            val getAllData = remRep.getAllData()
 
-                        title = {
-                            Text(
-                                "Home",
-                                maxLines = 1,
-                                overflow = TextOverflow.Visible
-                            )
-                        },
-                        navigationIcon = {
-                            val context = LocalContext.current
-                            IconButton(onClick = {context.startActivity(Intent(context,MainActivity::class.java))}) {
+            MobileTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
 
-                                Icon(
-                                    imageVector = Icons.Filled.ExitToApp,
-                                    contentDescription = "Log Out"
-                                )
-                            }
-                        },
-                        actions = {
-                            val context = LocalContext.current
-                            IconButton(onClick = {
-                                context.startActivity(Intent(context,MainActivity4::class.java))
-                                finish()
-                            }) {
-                                Icon(
-                                    imageVector = Icons.Filled.Add,
-                                    contentDescription = "Add"
-                                )
-                            }
-                            IconButton(onClick = {
-                                context.startActivity(Intent(context,MainActivity2::class.java))}) {
-                                Icon(
-                                    imageVector = Icons.Filled.Person,
-                                    contentDescription = "Profile"
-                                )
-                            }
-                        },
-
-                    )
-                },
-                content = {innerPadding ->
-                    LazyColumn(
-                        contentPadding = innerPadding,
-                        modifier = Modifier.fillMaxSize()
-
-                        ) {
-
-                            items(20) {
+                            title = {
                                 Text(
-                                    text = "Reminder $it",
-                                    fontSize = 24.sp,
-                                    fontWeight = FontWeight.Bold,
-                                    textAlign = TextAlign.Center,
-                                    modifier = Modifier
-                                        .fillMaxSize()
-                                        .padding(vertical = 12.dp)
+                                    "Home",
+                                    maxLines = 1,
+                                    overflow = TextOverflow.Visible
                                 )
+                            },
+                            navigationIcon = {
+                                val context = LocalContext.current
+                                IconButton(onClick = {context.startActivity(Intent(context,MainActivity::class.java))}) {
 
-                                Button(
-                                    onClick = {
-                                        //val intent = Intent(this, popupmenu::class.java)
-                                        //startActivity(intent)
-                                        //finish()
-                                    }
-                                ) {
-                                    Text(
-                                        text = "Edit",
-                                        textAlign = TextAlign.Center
+                                    Icon(
+                                        imageVector = Icons.Filled.ExitToApp,
+                                        contentDescription = "Log Out"
                                     )
                                 }
-
-                                Button(
-                                    onClick = {
-                                        //List.remove(item)
-                                    },
-                                ) {
-                                    Text(
-                                        text = "Delete",
-                                        textAlign = TextAlign.Center
+                            },
+                            actions = {
+                                val context = LocalContext.current
+                                IconButton(onClick = {
+                                    context.startActivity(Intent(context,MainActivity4::class.java))
+                                    finish()
+                                }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.Add,
+                                        contentDescription = "Add"
                                     )
                                 }
-                            }
+                                IconButton(onClick = {
+                                    context.startActivity(Intent(context,MainActivity2::class.java))}) {
+                                    Icon(
+                                        imageVector = Icons.Filled.Person,
+                                        contentDescription = "Profile"
+                                    )
+                                }
+                            },
+                            )
+                    },
+                )
+                { innerPadding ->
+                LazyColumn(contentPadding = innerPadding,
+                    verticalArrangement = Arrangement.spacedBy(12.dp))
+                {
+                    itemsIndexed(
+                        items = getAllData,
+                        key = {
+                            index, Reminder ->
+                            Reminder.creation_time
                         }
-                     })
-    }}}
+                    ){
+                        index,Reminder ->
+                        com.example.mobile.item(Reminder = Reminder)
+                    }
+                }
+                }
+            }
+            }
+    }
+}
--- app/src/main/java/com/example/mobile/repository/reminderRepository.kt
+++ app/src/main/java/com/example/mobile/repository/reminderRepository.kt
@@ -13,6 +13,79 @@ class reminderRepository {
                 creation_time= "5",
                 creator_id= "Wakeup",
                 reminder_seen= "Wakeup"
+            ) ,
+            reminder(
+            Message = "Meeting",
+            location_x= "Wakeup",
+            location_y= "Wakeup",
+            reminder_time = "7.00",
+            creation_time= "6",
+            creator_id= "Wakeup",
+            reminder_seen= "Wakeup"
+        ),
+            reminder(
+                Message = "Meeting",
+                location_x= "Wakeup",
+                location_y= "Wakeup",
+                reminder_time = "7.00",
+                creation_time= "7",
+                creator_id= "Wakeup",
+                reminder_seen= "Wakeup"
+            )
+            ,
+            reminder(
+                Message = "Meeting",
+                location_x= "Wakeup",
+                location_y= "Wakeup",
+                reminder_time = "7.00",
+                creation_time= "8",
+                creator_id= "Wakeup",
+                reminder_seen= "Wakeup"
+            ),
+            reminder(
+                Message = "Meeting",
+                location_x= "Wakeup",
+                location_y= "Wakeup",
+                reminder_time = "7.00",
+                creation_time= "9",
+                creator_id= "Wakeup",
+                reminder_seen= "Wakeup"
+            ),
+            reminder(
+                Message = "Meeting",
+                location_x= "Wakeup",
+                location_y= "Wakeup",
+                reminder_time = "7.00",
+                creation_time= "11",
+                creator_id= "Wakeup",
+                reminder_seen= "Wakeup"
+            ),
+            reminder(
+                Message = "Meeting",
+                location_x= "Wakeup",
+                location_y= "Wakeup",
+                reminder_time = "7.00",
+                creation_time= "12",
+                creator_id= "Wakeup",
+                reminder_seen= "Wakeup"
+            ),
+            reminder(
+                Message = "Meeting",
+                location_x= "Wakeup",
+                location_y= "Wakeup",
+                reminder_time = "7.00",
+                creation_time= "13",
+                creator_id= "Wakeup",
+                reminder_seen= "Wakeup"
+            ),
+            reminder(
+                Message = "Meeting",
+                location_x= "Wakeup",
+                location_y= "Wakeup",
+                reminder_time = "7.00",
+                creation_time= "14",
+                creator_id= "Wakeup",
+                reminder_seen= "Wakeup"
             )
         )
     }
